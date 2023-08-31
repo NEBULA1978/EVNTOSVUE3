d7360ed2a8862fbb0064444b32652e782b0747e3
NEBULA1978 <rpascualey@gmail.com>
2023-09-01 00:44:39 +0200

    Creando componentes con eventros en vue3

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import Von from './components/Von.vue'
+
+</script>
+
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<Von/>
+</template>
+<style scoped></style>
