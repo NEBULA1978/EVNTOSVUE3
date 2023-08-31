87b2fda66ebf3073a672a563bcd589e49b5bdae4
NEBULA1978 <rpascualey@gmail.com>
2023-09-01 00:53:35 +0200

    Creando componentes con eventos de botones y cintador en vue3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 import Von from './components/Von.vue'
+import VonParametros from './components/VonParametros.vue'
+import VariableReactivas from './components/VariableReactivas.vue'
+import VariableReactivas2 from './components/VariableReactivas2.vue'
+import Contador from './components/Contador.vue'
 
 </script>
 
 <template>
   <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 <Von/>
+<VonParametros/>
+<VariableReactivas/>
+<VariableReactivas2/>
+<Contador/>
 </template>
 <style scoped></style>
---
+++ src/components/VonParametros.vue
@@ -0,0 +1,20 @@
+<script setup>
+const name = "Vue 3";
+
+const handleClick = (message) => {
+    console.log(message);
+};
+</script>
+
+<template>
+    <h1>Hola {{ name }}!</h1>
+    <button @click="handleClick('Mensaje desde botón')">Click aquí</button>
+
+    <button @click.right.prevent="handleClick('Mensaje desde botón')">
+        Click right
+    </button>
+    <button @click.middle="handleClick('Mensaje desde botón')">
+        Click middle
+    </button>
+
+</template>
